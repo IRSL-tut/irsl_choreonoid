de28d63928cbac4e7be468b9319085098af34dcf
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2022-06-13 20:31:29 +0900

    Add PyAssimp

---
+++ pybind11/PyAssimpModule.cpp
@@ -0,0 +1,21 @@
+//#include "../AssimpSceneLoader.h"
+#include <pybind11/pybind11.h>
+#include <cnoid/PyUtil>
+#include <cnoid/PyEigenTypes>
+
+#include <cnoid/AssimpSceneLoader>
+
+using namespace cnoid;
+namespace py = pybind11;
+
+PYBIND11_MODULE(Assimp, m)
+{
+  m.doc() = "AssimpSceneLoader module";
+
+  py::module::import("cnoid.Util");
+
+  py::class_<AssimpSceneLoader>(m, "AssimpSceneLoader")
+    .def(py::init<>())
+    .def("load", &AssimpSceneLoader::load)
+    ;
+}
