c04e7ad67ced7c4954359689704c37f0a4b5f9d3
YoheiKakiuchi <kakiuchi.yohei.sw@tut.jp>
2022-06-21 20:27:23 +0900

    add coordinates

--- include/irsl_choreonoid/EigenUtil.h
+++ include/irsl_choreonoid/EigenUtil.h
@@ -5,11 +5,6 @@
 #define __irsl_use_inline__
 
 namespace cnoid {
-    enum coords_wrt {
-        default_wrt,
-        local,
-        world
-    };
 
     inline bool eps_eq(const double a, const double b, const double eps = 0.00001) {
         return fabs((a)-(b)) <= eps;
@@ -50,6 +45,7 @@ namespace cnoid {
     }
 
     // matrix product using quaternion normalization
+    // m12 <= m1 * m2
     inline void rotm3times (Matrix3& m12, const Matrix3& m1, const Matrix3& m2)
     {
         Quaternion eiq1(m1);
@@ -59,8 +55,23 @@ namespace cnoid {
         eiq3.normalize();
         m12 = eiq3.toRotationMatrix();
     }
+    // m1 <= m1 * m2
+    inline void rot_with_normalize (Matrix3& m1, const Matrix3& m2)
+    {
+        Quaternion eiq1(m1);
+        Quaternion eiq2(m2);
+        Quaternion eiq3;
+        eiq3 = eiq1 * eiq2;
+        eiq3.normalize();
+        m1 = eiq3.toRotationMatrix();
+    }
 
     inline void difference_rotation(Vector3& ret_dif_rot, const Matrix3& self_rot, const Matrix3& target_rot)
+    {
+        //ret_dif_rot = self_rot * hrp::omegaFromRot(self_rot.transpose() * target_rot);
+        ret_dif_rot = self_rot * matrix_log ( self_rot.transpose() * target_rot );
+    }
+    inline void difference_rotation__(Vector3& ret_dif_rot, const Matrix3& self_rot, const Matrix3& target_rot)
     {
         //ret_dif_rot = self_rot * hrp::omegaFromRot(self_rot.transpose() * target_rot);
         ret_dif_rot = self_rot * matrix_log ( self_rot.transpose() * target_rot );
@@ -90,29 +101,240 @@ namespace cnoid {
     }
 
 #endif
+
     // from rats coordinates
     // coordinates => Position(Isometry3d)
+    // copy from hrpsys-base/rtc/ImpedanceController/RatsMatrix
+    struct coordinates {
+        enum wrt {
+            default_wrt,
+            local,
+            world
+        };
+
+        Vector3 pos;
+        Matrix3 rot;
+
+        coordinates() : pos(Vector3::Zero()), rot(Matrix3::Identity()) {};
+        coordinates(const Vector3& p, const Matrix3& r) : pos(p), rot(r) {};
+        coordinates(const Vector3& p, const Quaternion& q) : pos(p), rot(q) {};
+        coordinates(const Vector3& p, const AngleAxis& ax) : pos(p), rot(ax) {};
+        coordinates(const Vector3& p) : pos(p), rot(Matrix3::Identity()) {};
+        coordinates(const Matrix3& r) : pos(Vector3::Zero()), rot(r) {};
+        coordinates(const Quaternion& q) : pos(Vector3::Zero()), rot(q) {};
+        coordinates(const AngleAxis& ax) : pos(Vector3::Zero()), rot(ax) {};
+        coordinates(const coordinates& c) : pos(c.pos), rot(c.rot) {};
+        coordinates(const Position &p) : pos(p.translation()), rot(p.linear()) {};
+        virtual ~coordinates() { }
+
+        void set(const Vector3 &p) { pos = p; }
+        void set(const Matrix3 &r) { rot = r; }
+        void set(const Quaternion &q) { rot = q; }
+        void set(const AngleAxis &ax) { rot = ax; }
+
+        void toPosition(Position &p) const
+        {
+            p.setIdentity();
+            p.translation() = pos;
+            p.linear() = rot;
+        }
+        coordinates& operator=(const coordinates& c)
+        {
+            if (this != &c) {
+                pos = c.pos;
+                rot = c.rot;
+            }
+            return *this;
+        }
+        coordinates& operator=(const Position& p)
+        {
+            pos = p.translation();
+            rot = p.linear();
+            return *this;
+        }
+        void rotate_with_matrix (const Matrix3& mat, const wrt wrt = local )
+        {
+            Matrix3 rot_org(rot);
+            if (wrt == local) {
+                // alias(rot) = rot * mat;
+                rotm3times(rot, rot_org, mat);
+            } else if(wrt == world) {
+                // alias(rot) = mat * rot;
+                rotm3times(rot, mat, rot_org);
+            } else {
+                //std::cerr << "**** invalid wrt! ****" << std::endl;
+            }
+        }
+        //void rotate_with_matrix (const Matrix3& mat, const coordinates &wrt)
+        void rotate (const double theta, const Vector3& axis, const wrt wrt = local )
+        {
+            Eigen::AngleAxis<double> tmpr(theta, axis);
+            rotate_with_matrix(tmpr.toRotationMatrix(), wrt);
+        }
+        //void rotate (const double theta, const Vector3& axis, const coordinates &wrt)
+        /* void difference_rotation(Vector3& dif_rot, const coordinates& c) const { */
+        /*   dif_rot = rot * matrix_log(rot.transpose() * c.rot); */
+        /* } */
+        void difference(Vector3& dif_pos, Vector3& dif_rot, const coordinates& c) const
+        {
+            dif_pos = c.pos - pos;
+            difference_rotation__(dif_rot, rot, c.rot);
+        }
+        void difference_position(Vector3& dif_pos, const coordinates& c) const
+        {
+            dif_pos = c.pos - pos;
+        }
+        void difference_rotation(Vector3& dif_rot, const coordinates& c) const
+        {
+            difference_rotation__(dif_rot, rot, c.rot);
+        }
+        //abc
+        void inverse_transformation(coordinates& inv) const
+        {
+            inv.rot = rot.transpose();
+            inv.pos = inv.rot*(-1 * pos);
+        }
+        //void transformation(coordinates& tc, coordinates c, const wrt wrt = local ) const
+        void transformation(coordinates& tc, const coordinates &c, const wrt wrt = local ) const
+        {
+#if 0
+            tc = *this;//??
+            inverse_transformation(tc);
+            if (wrt == local) {
+                tc.transform(c);
+            } else if(wrt == world) {
+                coordinates tmp(c);
+                tmp.transform(tc);
+                tc = tmp;
+            } else {
+                //std::cerr << "**** invalid wrt! ****" << std::endl;
+            }
+#endif
+            coordinates tmp(c);
+            inverse_transformation(tc);
+            tc.transform(tmp, wrt);
+        }
+        void transformation(coordinates& tc, const coordinates &c, const coordinates &wrt) const
+        {
+            coordinates tmp(c);
+            coordinates inv_self;
+            inverse_transformation(inv_self);
+
+            wrt.inverse_transformation(tc);
+            tc.transform(tmp);
+            tc.transform(inv_self);
+            tc.transform(wrt);
+        }
+        void transform(const coordinates& c, const wrt wrt = local )
+        {
+            if (wrt == local) {
+                pos += rot * c.pos;
+                // alias(rot) = rot * c.rot;
+                Matrix3 rot_org(rot);
+                rotm3times(rot, rot_org, c.rot);
+            } else if (wrt == world) {
+                Vector3 p(c.pos);
+                Matrix3 r(c.rot);
+                p += r * pos;
+                rotm3times(r, c.rot, rot);
+                pos = p;
+                rot = r;
+            } else {
+                //std::cerr << "**** invalid wrt! ****" << std::endl;
+            }
+        }
+        void transform(const coordinates& c, const coordinates &wrt )
+        {
+            coordinates tmp(wrt);
+            coordinates inv_wrt;
+            wrt.inverse_transformation(inv_wrt);
+            tmp.transform(c);
+            tmp.transform(inv_wrt);
+            tmp.transform(*this);
+
+            *this = tmp;
+        }
+
+        ////
+        void rotate_vector(Vector3 &vec) const
+        {
+            vec = rot * vec;
+        }
+        void inverse_rotate_vector(Vector3 &vec) const
+        {
+            vec = vec.transpose() * rot;
+        }
+        void transform_vector(Vector3 &vec) const
+        {
+            vec = rot * vec;
+            vec += pos;
+        }
+        void inverse_transform_vector(Vector3 &vec) const
+        {
+            vec = rot.transpose() * (vec - pos);
+        }
+        void move_to (const coordinates &c, const wrt wrt = local )
+        {
+            if (wrt == local) {
+                transform(c);
+            } else if (wrt == world) {
+                *this = c;
+            }
+        }
+        void move_to (const coordinates &c, const coordinates &wrt)
+        {
+            coordinates tmp(wrt);
+            tmp.transform(c);
+            *this = tmp;
+        }
+        void translate(const Vector3 &vec, const wrt wrt = local )
+        {
+            if (wrt == local) {
+                pos += rot * vec;
+            } else if (wrt == world) {
+                pos += vec;
+            }
+        }
+        void translate(const Vector3 &vec, const coordinates &wrt)
+        {
+            pos += wrt.rot * vec;
+        }
+        void locate(const Vector3 &vec, const wrt wrt = local )
+        {
+            if (wrt == local) {
+                pos += rot * vec;
+            } else if (wrt == world) {
+                pos = vec;
+            }
+        }
+        void locate(const Vector3 &vec, const coordinates &wrt)
+        {
+            pos = wrt.pos + (wrt.rot * vec);
+        }
+        // void orient(const Matrix3 &mat, const wrt wrt = local )
+    };
+
 #ifndef __irsl_use_inline__
     //void rotate_with_matrix (const hrp::Matrix33& mat, const std::string& wrt = ":local");
-    void rotate_with_matrix(Position &coords, const Matrix3 &mat, const coords_wrt wrt = local ); //wrt
+void rotate_with_matrix(Position &coords, const Matrix3 &mat, const coordinates::wrt wrt = coordinates::wrt::local ); //wrt
     //void rotate (const double theta, const hrp::Vector3& axis, const std::string& wrt = ":local");
-    void rotate(Position &coords, const double theta, const Vector3& axis, const coords_wrt wrt = local ); // wrt
+    void rotate(Position &coords, const double theta, const Vector3& axis, const coordinates::wrt wrt = coordinates::wrt::local ); // wrt
     //void difference(hrp::Vector3& dif_pos, hrp::Vector3& dif_rot, const coordinates& c);
     void difference(const Position &coords, Vector3& dif_pos, Vector3& dif_rot, const Position& c);
     //void inverse_transformation(coordinates& inv);
     void inverse_transformation(const Position &coords, Position& inv);
     //void transformation(coordinates& tc, coordinates c, const std::string& wrt = ":local") const;
-    void transformation(const Position &coords, Position& trans_coords, const Position &c, const coords_wrt wrt = local ); // wrt
+    void transformation(const Position &coords, Position& trans_coords, const Position &c, const coordinates::wrt wrt = coordinates::wrt::local ); // wrt
     //void transform(const coordinates& c, const std::string& wrt = ":local");
-    void transform(Position &coords, const Position& c, const coords_wrt wrt = local ); // wrt
+    void transform(Position &coords, const Position& c, const coordinates::wrt wrt = coordinates::wrt::local ); // wrt
 #else
-    inline void rotate_with_matrix(Position &coords, const Matrix3 &mat, const coords_wrt wrt = local )
+    inline void rotate_with_matrix(Position &coords, const Matrix3 &mat, const coordinates::wrt wrt = coordinates::wrt::local )
     {
         Matrix3 rot;
         Matrix3 rot_org = coords.linear();
-        if (wrt == local) {
+        if (wrt == coordinates::wrt::local) {
             rotm3times(rot, rot_org, mat);
-        } else if (wrt == world) {
+        } else if (wrt == coordinates::wrt::world) {
             rotm3times(rot, mat, rot_org);
         } else {
             return;
@@ -120,7 +342,7 @@ namespace cnoid {
         coords.linear() = rot;
     }
 
-    inline void rotate(Position &coords, const double theta, const Vector3& axis, const coords_wrt wrt = local )
+    inline void rotate(Position &coords, const double theta, const Vector3& axis, const coordinates::wrt wrt = coordinates::wrt::local )
     {
         AngleAxis tmpr(theta, axis);
         rotate_with_matrix(coords, tmpr.toRotationMatrix(), wrt);
@@ -131,21 +353,28 @@ namespace cnoid {
         dif_pos = c.translation() - coords.translation();
         difference_rotation(dif_rot, coords.linear(), c.linear());
     }
-
+    inline void difference_rotation(const Position &coords, Vector3& dif_rot, const Position& c)
+    {
+        difference_rotation(dif_rot, coords.linear(), c.linear());
+    }
+    inline void difference_position(const Position &coords, Vector3& dif_pos, const Position& c)
+    {
+        dif_pos = c.translation() - coords.translation();
+    }
     inline void inverse_transformation(const Position &coords, Position& inv)
     {
         inv = coords.inverse();
     }
 
-    inline void transform(Position &coords, const Position& c, const coords_wrt wrt = local )
+    inline void transform(Position &coords, const Position& c, const coordinates::wrt wrt = coordinates::wrt::local )
     {
-        if (wrt == local) {
+        if (wrt == coordinates::wrt::local) {
             Position tmp = (coords * c);
             Matrix3 rot;
             rotm3times(rot, coords.linear(), c.linear());
             coords.translation() = tmp.translation();
             coords.linear() = rot;
-        } else if (wrt == world) {
+        } else if (wrt == coordinates::wrt::world) {
             Position tmp = (c * coords);
             Matrix3 rot;
             rotm3times(rot, c.linear(), coords.linear());
@@ -154,33 +383,30 @@ namespace cnoid {
         }
     }
 
-    inline void transformation(const Position &coords, Position& trans_coords, const Position &c, const coords_wrt wrt = local )
+    inline void transformation(const Position &coords, Position& trans_coords, const Position &c, const coordinates::wrt wrt = coordinates::wrt::local )
     {
-        if(wrt == local) {
+#if 0
+        if(wrt == coordinates::wrt::local) {
             // coords^-1 * c
             inverse_transformation(coords, trans_coords);
             transform(trans_coords, c);
-        } else if (wrt == world) {
+        } else if (wrt == coordinates::wrt::world) {
             // c * coords^-1
             Position inv_trans;
             inverse_transformation(coords, inv_trans);
             trans_coords = c;
             transform(trans_coords, inv_trans);
         }
+#endif
+        Position tmp(c);
+        inverse_transformation(coords, trans_coords);
+        transform(trans_coords, tmp, wrt);
     }
 
 #endif
-    //from EusLisp definition
-    //rotate_vector
-    //inverse_rotate_vector
-    //transform_vector
-    //inverse_transform_vector
-    //
-    //--transformation :local :world :wrt
-    //inverse_transformation :local :world :wrt
-    //
-    //move-to :local :world :wrt
-    //translate :local :world :wrt
-    //locate :local :world :wrt
-    //--transform  :local :world :wrt  WAW^-1 T
+    inline void mid_coords(coordinates& mid_coords, const double p, const coordinates& c1, const coordinates& c2, const double eps = 0.00001)
+    {
+        mid_coords.pos = (1 - p) * c1.pos + p * c2.pos;
+        mid_rot(mid_coords.rot, p, c1.rot, c2.rot, eps);
+    };
 }
--- pybind11/PyIRSLUtil.cpp
+++ pybind11/PyIRSLUtil.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <memory>
+#include <sstream>
 #include <pybind11/pybind11.h>
 
 #include "irsl_choreonoid/EigenUtil.h"
@@ -15,11 +16,16 @@ namespace py = pybind11;
 using Matrix4RM = Eigen::Matrix<double, 4, 4, Eigen::RowMajor>;
 using Matrix3RM = Eigen::Matrix<double, 3, 3, Eigen::RowMajor>;
 
+// numpy (RowMajor) / Eigen (ColumnMajor)
+// Ref<T> for a writable reference,
+// const Ref<const T>& for a const reference
+// Ref<const T>, might still be interesting if you want to change the matrix that is referenced (not its content) through a call to Ref constructor using placement new.
 typedef Eigen::Ref<const Matrix4RM> ref_mat4;
 typedef Eigen::Ref<const Matrix3RM> ref_mat3;
 typedef Eigen::Ref<const Vector4>   ref_vec4;
 typedef Eigen::Ref<const Vector3>   ref_vec3;
 
+typedef std::shared_ptr< coordinates > coordinatesPtr;
 
 Matrix4RM mid_coords_(const double p, ref_mat4 c1, ref_mat4 c2, const double eps)
 {
@@ -38,7 +44,7 @@ PYBIND11_MODULE(IRSLUtil, m)
 
     m.def("mid_coords", mid_coords_, py::arg("p"), py::arg("c1"), py::arg("c2"), py::arg("eps") = 0.00001);
 
-    m.def("PositionInverse", [](ref_mat4 in_p) { Position p(in_p); return p.inverse().matrix(); });
+    m.def("PositionInverse", [](ref_mat4 in_p) -> Matrix4RM { Position p(in_p); return p.inverse().matrix(); });
     m.def("Position_translation", [](ref_mat4 in_p) { Position p(in_p); return Vector3(p.translation()); });
     m.def("Position_quaternion", [](ref_mat4 in_p) { Quaternion q(Position(in_p).linear());
             return Vector4(q.x(), q.y(), q.z(), q.w()); });
@@ -47,15 +53,15 @@ PYBIND11_MODULE(IRSLUtil, m)
             return Vector4(q.x(), q.y(), q.z(), q.w()); });
     m.def("quaternionToRotation", [](ref_vec4 q) { return Matrix3RM(Quaternion(q)); });
 
-    m.def("cnoidPosition", [](ref_mat3 rot, ref_vec3 trans) -> Isometry3::MatrixType {
+    m.def("cnoidPosition", [](ref_mat3 rot, ref_vec3 trans) -> Matrix4RM {
             Position ret; ret.setIdentity(); ret.linear() = rot; ret.translation() = trans; return ret.matrix(); });
-    m.def("cnoidPosition", [](ref_vec4 q, ref_vec3 trans) -> Isometry3::MatrixType {
+    m.def("cnoidPosition", [](ref_vec4 q, ref_vec3 trans) -> Matrix4RM {
             Position ret; ret.setIdentity(); ret.linear() = Matrix3(Quaternion(q)); ret.translation() = trans; return ret.matrix(); });
-    m.def("cnoidPosition", [](ref_vec3 trans) -> Isometry3::MatrixType {
+    m.def("cnoidPosition", [](ref_vec3 trans) -> Matrix4RM {
             Position ret; ret.setIdentity(); ret.translation() = trans; return ret.matrix(); });
-    m.def("cnoidPosition", [](ref_mat3 rot) -> Isometry3::MatrixType {
+    m.def("cnoidPosition", [](ref_mat3 rot) -> Matrix4RM {
             Position ret; ret.setIdentity(); ret.linear() = rot; return ret.matrix(); });
-    m.def("cnoidPosition", [](ref_vec4 q) -> Isometry3::MatrixType {
+    m.def("cnoidPosition", [](ref_vec4 q) -> Matrix4RM {
             Position ret; ret.setIdentity(); ret.linear() = Matrix3(Quaternion(q)); return ret.matrix(); });
 
     m.def("normalizeVector", [](ref_vec3 v3) { return v3.normalized(); });
@@ -63,44 +69,153 @@ PYBIND11_MODULE(IRSLUtil, m)
 
     m.def("angleAxisNormalized", [](double angle, ref_vec3 axis) {
             Vector3 ax_ = axis.normalized(); return Matrix3RM(AngleAxis(angle, ax_)); });
-    m.def("Position_rotate_with_matrix", [](ref_mat4 cds, ref_mat3 mat, const std::string &wrt) -> Isometry3::MatrixType {
+    m.def("Position_rotate_with_matrix", [](ref_mat4 cds, ref_mat3 mat, const std::string &wrt) -> Matrix4RM {
             Position coords(cds);
             Matrix3 mat_(mat);
             if (wrt == "local") {
-                rotate_with_matrix(coords, mat_, local);
+                rotate_with_matrix(coords, mat_, coordinates::wrt::local);
             } else if (wrt == "world") {
-                rotate_with_matrix(coords, mat_, world);
+                rotate_with_matrix(coords, mat_, coordinates::wrt::world);
             }
             return coords.matrix(); },
         py::arg("cds"), py::arg("mat"), py::arg("wrt") = "local");
-    m.def("Position_rotate", [](ref_mat4 cds, const double theta, ref_vec3 axis, const std::string &wrt) -> Isometry3::MatrixType {
+    m.def("Position_rotate", [](ref_mat4 cds, const double theta, ref_vec3 axis, const std::string &wrt) -> Matrix4RM {
             Position coords(cds);
             if (wrt == "local") {
-                rotate(coords, theta, axis, local);
+                rotate(coords, theta, axis, coordinates::wrt::local);
             } else if (wrt == "world") {
-                rotate(coords, theta, axis, world);
+                rotate(coords, theta, axis, coordinates::wrt::world);
             }
             return coords.matrix(); },
         py::arg("cds"), py::arg("theta"), py::arg("axis"), py::arg("wrt") = "local");
-    m.def("Position_transform", [](ref_mat4 cds, ref_mat4 c, const std::string &wrt) -> Isometry3::MatrixType {
+    m.def("Position_transform", [](ref_mat4 cds, ref_mat4 c, const std::string &wrt) -> Matrix4RM {
             Position coords(cds);
             Position c_(c);
             if (wrt == "local") {
-                transform(coords, c_, local);
+                transform(coords, c_, coordinates::wrt::local);
             } else if (wrt == "world") {
-                transform(coords, c_, world);
+                transform(coords, c_, coordinates::wrt::world);
             }
             return coords.matrix(); },
         py::arg("cds"), py::arg("c"), py::arg("wrt") = "local");
-    m.def("Position_transformation", [](ref_mat4 cds, ref_mat4 c, const std::string &wrt) -> Isometry3::MatrixType {
+    m.def("Position_transformation", [](ref_mat4 cds, ref_mat4 c, const std::string &wrt) -> Matrix4RM {
             Position coords(cds);
             Position c_(c);
             Position trans_c;
             if (wrt == "local") {
-                transformation(coords, trans_c, c_, local);
+                transformation(coords, trans_c, c_, coordinates::wrt::local);
             } else if (wrt == "world") {
-                transformation(coords, trans_c, c_, world);
+                transformation(coords, trans_c, c_, coordinates::wrt::world);
             }
             return trans_c.matrix(); },
         py::arg("cds"), py::arg("c"), py::arg("wrt") = "local");
+
+    py::class_< coordinates, coordinatesPtr > coords_cls(m, "coordinates");
+
+    py::enum_<coordinates::wrt>(coords_cls, "wrt")
+    .value("local", coordinates::wrt::local)
+    .value("world", coordinates::wrt::world)
+    .export_values();
+
+    coords_cls.def(py::init<>())
+    .def(py::init([](ref_vec3 trs, ref_mat3 rot)
+                  { return new coordinates(trs, rot); }))
+    .def(py::init([](ref_vec3 trs)
+                  { Vector3 v(trs); return new coordinates(v); }))
+    .def(py::init([](ref_mat3 rot)
+                  { Matrix3 m(rot); return new coordinates(m); }))
+    .def(py::init([](ref_vec4 rot)
+                  { Quaternion q(rot); return new coordinates(q); }))
+    .def(py::init([](ref_vec3 trs, ref_vec4 rot)
+                  { Quaternion q(rot); return new coordinates(trs, q); }))
+    .def(py::init([](ref_mat4 position)
+                  { Position p(position); return new coordinates(p); }))
+    .def("__repr__", [](const coordinates &self) {
+            std::stringstream ss;
+            ss << "<coordinates ";
+            Quaternion q(self.rot);
+            ss << self.pos(0); ss << " ";
+            ss << self.pos(1); ss << " ";
+            ss << self.pos(2); ss << " / ";
+            ss << q.x(); ss << " ";
+            ss << q.y(); ss << " ";
+            ss << q.z(); ss << " ";
+            ss << q.w(); ss << " >";
+            return ss.str();
+        })
+    .def_property("pos",
+                  [](coordinates &self) { return self.pos; },
+                  [](coordinates &self, ref_vec3 vec) { self.pos = vec; })
+    .def_property("rot",
+                  [](coordinates &self) { return self.rot; },
+                  [](coordinates &self, ref_mat3 mat) { self.rot = mat; })
+    .def("toPosition",
+         [](const coordinates &self) -> Matrix4RM { Position p; self.toPosition(p); return p.matrix(); })
+    .def("rotate_with_matrix",
+         [](coordinates &self, ref_mat3 mat, coordinates::wrt wrt)
+         {  self.rotate_with_matrix(mat, wrt); },
+         py::arg("mat"), py::arg("wrt") = coordinates::wrt::local )
+    .def("rotate",
+         [](coordinates &self, double th_, ref_vec3 ax_, coordinates::wrt wrt)
+         { self.rotate(th_, ax_, wrt); },
+         py::arg("theta"), py::arg("axis"), py::arg("wrt") = coordinates::wrt::local )
+    .def("difference_rotation",
+         [](const coordinates &self, coordinates &c) {
+             Vector3 ret; self.difference_rotation(ret, c); return ret;
+         } )
+    .def("difference_position",
+         [](const coordinates &self, coordinates &c) {
+             Vector3 ret; self.difference_position(ret, c); return ret;
+         } )
+    .def("inverse_transformation",
+         [](const coordinates &self) {
+             coordinates ret; self.inverse_transformation(ret); return ret;
+         } )
+    .def("transformation",
+         [](const coordinates &self, const coordinates &c, coordinates::wrt wrt) {
+             coordinates ret; self.transformation(ret, c, wrt); return ret;
+         }, py::arg("coords"), py::arg("wrt") = coordinates::wrt::local )
+    .def("transformation",
+         [](const coordinates &self, const coordinates &c, const coordinates &wrt) {
+             coordinates ret; self.transformation(ret, c, wrt); return ret;
+         } )
+    .def("transform",
+         [](coordinates &self, const coordinates &c, coordinates::wrt wrt)
+         { self.transform(c, wrt); }, py::arg("coords"), py::arg("wrt") = coordinates::wrt::local )
+    .def("transform",
+         [](coordinates &self, const coordinates &c, const coordinates &wrt)
+         { self.transform(c, wrt); } )
+    ////
+    .def("rotate_vector",
+         [](const coordinates &self, Eigen::Ref<Vector3> vec) { Vector3 v(vec);
+             self.rotate_vector(v); vec = v; return vec; })
+    .def("inverse_rotate_vector",
+         [](const coordinates &self, Eigen::Ref<Vector3> vec) { Vector3 v(vec);
+             self.inverse_rotate_vector(v); vec = v; return vec; })
+    .def("transform_vector",
+         [](const coordinates &self, Eigen::Ref<Vector3> vec) { Vector3 v(vec);
+             self.transform_vector(v); vec = v; return vec; })
+    .def("inverse_transform_vector",
+         [](const coordinates &self, Eigen::Ref<Vector3> vec) { Vector3 v(vec);
+             self.inverse_transform_vector(v); vec = v; return vec; })
+    ///
+    .def("translate",
+         [](coordinates &self, ref_vec3 vec, coordinates::wrt wrt) {
+             self.translate(vec, wrt); }, py::arg("vector"), py::arg("wrt") = coordinates::wrt::local )
+    .def("translate",
+         [](coordinates &self, ref_vec3 vec, const coordinates &wrt) {
+             self.translate(vec, wrt); } )
+    .def("locate",
+         [](coordinates &self, ref_vec3 vec, coordinates::wrt wrt) {
+             self.locate(vec, wrt); }, py::arg("vector"), py::arg("wrt") = coordinates::wrt::local )
+    .def("locate",
+         [](coordinates &self, ref_vec3 vec, const coordinates &wrt) {
+             self.locate(vec, wrt); } )
+    .def("move_to",
+         [](coordinates &self, const coordinates &vec, coordinates::wrt wrt) {
+             self.move_to(vec, wrt); }, py::arg("vector"), py::arg("wrt") = coordinates::wrt::local )
+    .def("move_to",
+         [](coordinates &self, const coordinates &vec, const coordinates &wrt) {
+             self.move_to(vec, wrt); } )
+    ;
 }
