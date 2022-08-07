a810bd6ff82bcc5525cfacb75ce95971696b1c1b
YoheiKakiuchi <kakiuchi.yohei.sw@tut.jp>
2022-08-07 18:37:36 +0900

    [WIP, assembler] class refactor / start

--- plugins/AssemblerFileIO.h
+++
@@ -1,7 +0,0 @@
-#ifndef CNOID_ROBOT_ASSEMBLER_PLUGIN_BODY_ITEM_FILEIO_H
-#define CNOID_ROBOT_ASSEMBLER_PLUGIN_BODY_ITEM_FILEIO_H
-
-#include "AssemblerBodyItem.h"
-#include <cnoid/ItemFileIO>
-
-#endif
