d7d854d1f4805a315288b349d9c8e5b6bc5f7166
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-09-29 10:40:22 +0900

    FIX: [python/make_shapes.py] add dtype for numpy.array

--- python/irsl_choreonoid/make_shapes.py
+++ python/irsl_choreonoid/make_shapes.py
@@ -25,22 +25,22 @@ def generateMaterial(**kwargs):
 
     val = __gets(('DiffuseColor', 'diffusecolor', 'diffuse-color', 'diffuse'), kwargs)
     if val is not None:
-        mat.setDiffuseColor(npa(val))
+        mat.setDiffuseColor(npa(val, dtype='float64'))
         valueset = True
 
     val = __gets(('EmissiveColor', 'emissivecolor', 'emissive-color', 'emissive'), kwargs)
     if val is not None:
-        mat.setEmissiveColor(npa(val))
+        mat.setEmissiveColor(npa(val, dtype='float64'))
         valueset = True
 
     val = __gets(('SpecularExponent', 'specularexponent', 'specular-exponent'), kwargs)
     if val is not None:
-        mat.setSpecularExponent(val)
+        mat.setSpecularExponent(val, dtype='float64')
         valueset = True
 
     val = __gets(('SpecularColor', 'specularcolor', 'specular-color', 'specular'), kwargs)
     if val is not None:
-        mat.setSpecularColor(npa(val))
+        mat.setSpecularColor(npa(val, dtype='float64'))
         valueset = True
 
     val = __gets(('Transparency', 'transparency', 'Transparent', 'transparent'), kwargs)
@@ -51,8 +51,8 @@ def generateMaterial(**kwargs):
     val = __gets(('color', 'Color'), kwargs)
     if val is not None:
         mat.setAmbientIntensity(1.0)
-        mat.setDiffuseColor(npa(val) * 0.7)
-        mat.setEmissiveColor(npa(val) * 0.3)
+        mat.setDiffuseColor(npa(val, dtype='float64') * 0.7)
+        mat.setEmissiveColor(npa(val, dtype='float64') * 0.3)
         valueset = True
 
     if valueset:
@@ -191,12 +191,12 @@ def makeBox(x, y = None, z = None, wrapped=True, coords=None, **kwargs):
     """
     mg = cutil.MeshGenerator()
     parseMeshGeneratorOption(mg, **kwargs)
-    if type(x) is numpy.ndarray:
-        mesh = mg.generateBox(x)
+    if type(x) is numpy.ndarray or type(x) is list:
+        mesh = mg.generateBox(npa(x, dtype='float64'))
     elif y is not None and z is not None:
-        mesh = mg.generateBox(npa([x, y, z]))
+        mesh = mg.generateBox(npa([x, y, z], dtype='float64'))
     elif type(x) is int or type(x) is float:
-        mesh = mg.generateBox(npa([x, x, x]))
+        mesh = mg.generateBox(npa([x, x, x], dtype='float64'))
     else:
         raise Exception(f'Invalid arguments x: {x}, y: {y}, z: {z}')
 
@@ -311,12 +311,12 @@ def makeTorus(radius, corssSectionRadius, beginAngle = None, endAngle = None, wr
 
 def _makeExtrusionParam(crossSection, spine, orientation=None, scale=None, creaseAngle=None, beginCap=None, endCap=None, **kwargs):
     extconf = cutil.MeshGenerator.Extrusion()
-    extconf.crossSection = npa(crossSection)
-    extconf.spine        = npa(spine)
+    extconf.crossSection = npa(crossSection, dtype='float64')
+    extconf.spine        = npa(spine, dtype='float64')
     if orientation is not None:
         extconf.orientation=orientation
     if scale is not None:
-        extconf.scale=npa(scale)
+        extconf.scale=npa(scale, dtype='float64')
     if creaseAngle is not None:
         extconf.creaseAngle=creaseAngle
     if beginCap is not None:
