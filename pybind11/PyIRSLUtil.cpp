01cf7b62ec43755c4c8e904c7a371be8e5e0ecb7
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-06-18 19:08:01 +0900

    [PyIRSLUtil.cpp] add fixSgPosTransform and fixMesh

--- pybind11/PyIRSLUtil.cpp
+++ pybind11/PyIRSLUtil.cpp
@@ -9,6 +9,10 @@
 #include <cnoid/PyEigenTypes>
 #include <cnoid/ViewManager>
 
+//
+#include <cnoid/SceneGraph>
+#include <cnoid/SceneDrawables>
+
 #include <vector>
 #include <algorithm>
 
@@ -43,6 +47,32 @@ void unmount_viewtitles(std::vector<std::string> &_in)
     }
 }
 
+void fixSgPosTransform(SgPosTransform &_trs)
+{
+    Matrix4 T = _trs.T().matrix();
+    T(0, 2) = -T(0, 2);
+    T(1, 2) = -T(1, 2);
+    T(2, 2) = -T(2, 2);
+    Isometry3 newT(T);
+    _trs.setPosition(newT);
+}
+
+void fixMesh(SgMesh &_mesh)
+{
+    if(_mesh.hasNormals()) {
+        SgNormalArray *nm = _mesh.normals();
+        for(auto it = nm->begin(); it != nm->end(); it++) {
+            (*it)(2) = - (*it)(2);
+        }
+    }
+    if(_mesh.hasVertices()) {
+        SgVertexArray *vt = _mesh.vertices();
+        for(auto it = vt->begin(); it != vt->end(); it++) {
+            (*it)(2) = - (*it)(2);
+        }
+    }
+}
+
 PYBIND11_MODULE(IRSLUtil, m)
 {
     m.doc() = "utilitiy for choreonoid (IRSL)";
@@ -57,4 +87,6 @@ PYBIND11_MODULE(IRSLUtil, m)
     });
 
     m.def("unmountViewTitles", &unmount_viewtitles);
+    m.def("fixSgPosTransform", &fixSgPosTransform);
+    m.def("fixMesh", &fixMesh);
 }
